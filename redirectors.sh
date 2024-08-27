9b3e60e721c77984204b502bd77390ecf4938a62
naveenreddycloud <devopscloud431@gmail.com>
2024-08-27 21:06:31 +0530

    redir

--- redirectors.sh
+++ redirectors.sh
@@ -28,7 +28,7 @@ VALIDATE(){
         echo  "$2 is...FAILED"  
         exit 1
     else
-        echo "$2 is...  SUCCESS" 
+        echo "$2 is...SUCCESS" 
     fi
 }
 
@@ -43,3 +43,13 @@ VALIDATE $? "enabled mysql server"
 
 systemctl start mysqld
 VALIDATE $? "started mysqld"
+
+mysql -h mysql.naveenreddy.online -u root -pExpenseApp@1
+if [ $? -ne 0 ]
+    then 
+        echo " MySQL root password is not setup, setting now"
+        mysql_secure_installation --set-root-pass ExpenseApp@1
+        VALIDATE $? "setting up root password"
+    else
+        echo "MySQL root password is already setup... SKIPPING"
+fi
