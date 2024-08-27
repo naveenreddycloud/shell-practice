d95ae2969b1b9d6bcd00c5d844d91079f38f98a8
naveenreddycloud <devopscloud431@gmail.com>
2024-08-27 20:19:50 +0530

    redir

--- redirectors.sh
+++ redirectors.sh
@@ -25,12 +25,15 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo  "is...FAILED"  
+        echo  "$2 is...FAILED"  
         exit 1
     else
-        echo "is...  SUCCESS" 
+        echo "$2 is...  SUCCESS" 
     fi
 }
 
 echo "Script started executing at: $(date)"
 CHECK_ROOT
+
+dnf install mysql server -y 
+VALIDATE $? 
