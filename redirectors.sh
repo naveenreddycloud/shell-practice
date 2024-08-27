642318d14cda1fd7cb0e98bdda8926cfa93ffe05
naveenreddycloud <devopscloud431@gmail.com>
2024-08-27 19:42:05 +0530

    redir

---
+++ redirectors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo  "Please run this script with root priveleges" 
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo  "is...FAILED"  
+        exit 1
+    else
+        echo "is...  SUCCESS" 
+    fi
+}
+
+echo "Script started executing at: $(date)"
