004f1c8c2e48edbbc5a85eac44a767a28a08cd59
naveenreddycloud <devopscloud431@gmail.com>
2024-09-01 09:00:21 +0530

    wctask

---
+++ taskassignments/task1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FILEPATH=$1
+SEARCH_WORD=$2
+USAGE(){
+        echo "USAGE:: sh <filename.sh> <argument1> <argument2>"
+        exit 1
+}
+if [ $# -lt 2 ]
+then
+        USAGE
+fi
+if [ -f $FILEPATH ]
+then
+        echo "File exist"
+else
+        echo "File Doesn't exit"
+fi
+count=$(cat $FILEPATH | grep $SEARCH_WORD | wc -l)
+echo "$SEARCH_WORD "=" $count"
